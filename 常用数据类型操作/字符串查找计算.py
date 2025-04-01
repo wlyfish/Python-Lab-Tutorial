94df62b7aee166fa40b95244e8a5a5a8449bdc0e
28250 <2825024660@qq.com>
2025-04-01 22:54:58 +0800

    字符串练习

---
+++ 常用数据类型操作/字符串查找计算.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 2025/4/1 22:10
+ @Author  : wly
+ @File    : 字符串查找计算.py
+ @Description: 
+"""
+str1 = 'wefgdafwedr\n'
+print(len(str1))
+print(str1.find('da'))
+print(str1.rfind('w'))
+
+print(str1.index('f'))
+
+print(str1.count('w'))
---
+++ 常用数据类型操作/字符串转换操作.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 2025/4/1 22:49
+ @Author  : wly
+ @File    : 字符串转换操作.py
+ @Description: 
+"""
+str1 = 'weouhyoqeurf8dsaf908u'
+print(str1.replace('o', '99', 1))
+print(str1.capitalize())
+str2 = 'weo uhy oqeu rf 8dsa f90 8u'
+print(str2.title())
+print(str2.upper())
